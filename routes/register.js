c1550fe448c0c3cb5e4e210abc4323a3b23dd224
baha908 <baho908@gmail.com>
2021-01-17 05:08:01 +0300

    update

--- routes/register.js
+++ routes/register.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const sqlite = require('sqlite3');
+const sqlite = require('sqlite3').verbose();
 const router = express.Router();
 
 /**
@@ -21,7 +21,7 @@ router.post('/register', (req,res) => {
             res.status(400).json({durumKodu: 400, mesaj:"Böyle bir kullanıcı daha önce kayıt olmuş"});
         else{ 
             const queryString = "INSERT INTO User ( Tc, Name, LastName, Email, Phone, PictureUrl, Twitter, Instagram, WebSite, Facebook, Password ) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
-            connection.all(queryString,[
+            connection.run(queryString,[
                 req.body.Tc,
                 req.body.Name,
                 req.body.LastName,
@@ -33,12 +33,12 @@ router.post('/register', (req,res) => {
                 req.body.WebSite,
                 req.body.Facebook,
                 req.body.Password
-            ], (err,rows,fields) => {
+            ], function (err,rows,fields) {
                 if(err){
                     res.status(400).json({statusCode: 400, message:err.message});
                 }
                 
-                res.status(201).json({statusCode:201, message: "Kullanıcı başarıyla oluşturuldu, giriş yapabilirsiniz"});
+                res.status(201).json({statusCode:200, message: "Kullanıcı başarıyla oluşturuldu, giriş yapabilirsiniz", userId: this.lastID});
             });
         }
         
